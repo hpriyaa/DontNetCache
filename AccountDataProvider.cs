625b0b7fc5228612abb987597bb5c31b333ca14a
hpriyaa <67458876+hpriyaa@users.noreply.github.com>
2023-09-12 18:19:10 +0530

    Add files via upload

---
+++ AccountDataProvider.cs
@@ -0,0 +1,37 @@
+﻿using ALEHA_API.Models;
+
+namespace ALEHA_API.Repository
+{
+    public class AccountDataProvider : IAccountDataProvider<Account>
+    {
+        private readonly AlehabankDbContext _context;
+        public AccountDataProvider(AlehabankDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool IsAccountEmpty()
+        {
+            if (_context.Accounts == null)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public int AddAccountDetails(Account account)
+        {
+            if (_context.Accounts != null)
+            {
+                _context.Accounts.Add(account);
+                _context.SaveChanges();
+                return account.AccountNumber;
+            }
+            else return 0;
+        }
+        //        public Account GetAccountDetail(Account account)
+        //       {
+        //           return _context.account.SingleOrDefault(x => x.Username == login.Username && x.Password == login.Password);
+        //       }
+    }
+}
---
+++ IAddAccountService.cs
@@ -0,0 +1,9 @@
+﻿using ALEHA_API.Models;
+
+namespace ALEHA_API.Services
+{
+    public interface IAddAccountService<T>
+    {
+        public string Add(Account account);
+    }
+}
