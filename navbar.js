323ee56de14d37f35c8efd567a4d4e1b675b8876
hpriyaa <67458876+hpriyaa@users.noreply.github.com>
2023-09-18 17:57:58 +0530

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CustomNavbar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Your Company Name</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    {/* Add your navigation links here */}
+                </Nav>
+                <NavDropdown title="Welcome, Customer Name" id="basic-nav-dropdown">
+                    {/* Add dropdown menu items */}
+                    <NavDropdown.Item href="#action">Profile</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/2">Logout</NavDropdown.Item>
+                </NavDropdown>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default CustomNavbar;
