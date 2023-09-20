07d74c9c0df352e3267283eb2eb5672a782a4797
hpriyaa <67458876+hpriyaa@users.noreply.github.com>
2023-09-20 12:46:09 +0530

    Update AccountDropDown.js

--- AccountDropDown.js
+++ AccountDropDown.js
@@ -1,38 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
 
-function AccountDropdown() {
+function AccountDropdown({ onSelectAccount }) {
     const [accounts, setAccounts] = useState([]);
     const [selectedAccount, setSelectedAccount] = useState('');
 
     useEffect(() => {
-        // Fetch accounts from your C# API (replace with your API endpoint)
+        // Fetch the list of accounts from your C# API (replace with your API endpoint)
         fetch('/api/accounts')
-            .then((response) => response.json())
-            .then((data) => setAccounts(data))
-            .catch((error) => console.error('Error fetching accounts:', error));
+            .then(response => response.json())
+            .then(data => {
+                setAccounts(data);
+            })
+            .catch(error => console.error('Error fetching accounts:', error));
     }, []);
 
-    const handleAccountSelect = (event) => {
-        setSelectedAccount(event.target.value);
+    const handleAccountChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedAccount(selectedValue);
+        onSelectAccount(selectedValue);
     };
 
     return (
-        <div>
-            <label htmlFor="accountDropdown">Select an Account:</label>
-            <select
-                id="accountDropdown"
-                className="form-control"
+        <Form.Group>
+            <Form.Label>Select an Account</Form.Label>
+            <Form.Control
+                as="select"
                 value={selectedAccount}
-                onChange={handleAccountSelect}
+                onChange={handleAccountChange}
             >
                 <option value="">Select an account</option>
-                {accounts.map((account) => (
+                {accounts.map(account => (
                     <option key={account.id} value={account.id}>
-                        {account.name}
+                        {account.accountName}
                     </option>
                 ))}
-            </select>
-        </div>
+            </Form.Control>
+        </Form.Group>
     );
 }
 
